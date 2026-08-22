6d50a1a245839f09c7cbee0d7c24c04120009ce3
downjavu <projectwebfirman@gmail.com>
2026-08-22 22:30:57 +0700

    fix: aktifkan adapter cloudflare untuk build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-// import cloudflare from '@astrojs/cloudflare';
+import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  // output: 'server',
-  // adapter: cloudflare(),
+  output: 'server',
+  adapter: cloudflare(),
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
