e3aaf5b70a2551c73f16c12a8a48c59ab8d380b9
downjavu <projectwebfirman@gmail.com>
2026-08-23 20:21:36 +0700

    edit kasir

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  output: 'static', // Ubah ke static
+  output: 'server',
+  adapter: cloudflare({
+    imageService: 'compile',
+  }),
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
